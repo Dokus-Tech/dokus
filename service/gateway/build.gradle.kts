b0f25be762e1094c871d4bea0fd5eebb6a66eb02
Artem Kuznetsov <artem@invoid.vision>
2024-12-17 20:26:27 +0100

    feat: setup dep for gateway

--- service/gateway/build.gradle.kts
+++ service/gateway/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlinxRpcPlugin)
+    alias(libs.plugins.kotlinPluginSerialization)
     application
 }
 
@@ -52,6 +54,12 @@ dependencies {
     implementation(libs.logback)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
-//    testImplementation(libs.ktor.server.tests)
+    implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.kotlinx.rpc.krpc.ktor.server)
+    implementation(libs.kotlinx.rpc.krpc.ktor.client)
+    implementation(libs.kotlinx.rpc.krpc.serialization.json)
+    testImplementation(libs.ktor.server.tests)
     testImplementation(libs.kotlin.test.junit)
 }
