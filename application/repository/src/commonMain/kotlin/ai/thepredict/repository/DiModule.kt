592071a7ea76e2309d9b3c1d3c1d561be744acd2
Artem Kuznetsov <artem@invoid.vision>
2024-12-25 15:31:03 +0100

    feat: client

--- application/repository/src/commonMain/kotlin/ai/thepredict/repository/DiModule.kt
+++ application/repository/src/commonMain/kotlin/ai/thepredict/repository/DiModule.kt
@@ -2,14 +2,10 @@ package ai.thepredict.repository
 
 import ai.thepredict.configuration.ServerEndpoint
 import ai.thepredict.repository.api.UnifiedApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.kodein.di.DI
 import org.kodein.di.singleton
 
 val repositoryDiModule by DI.Module("repository") {
-    GlobalScope.launch { // TODO: Get rid of global scope
-//        val api = UnifiedApi.create(ServerEndpoint.Gateway())
-//        singleton { api }
-    }
+    val api = UnifiedApi.create(ServerEndpoint.Gateway())
+    singleton { api }
 }
