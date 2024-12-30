9b889b63346452314f29104847eda9f500cdf594
Artem Kuznetsov <artem@invoid.vision>
2024-12-30 21:49:42 +0100

    chore: relations

--- application/onboarding/src/commonMain/kotlin/ai/thepredict/app/onboarding/login/LoginViewModel.kt
+++ application/onboarding/src/commonMain/kotlin/ai/thepredict/app/onboarding/login/LoginViewModel.kt
@@ -15,6 +15,7 @@ internal class LoginViewModel : StateScreenModel<LoginViewModel.State>(State.Loa
 
     fun fetch() {
         screenModelScope.launchStreamScoped {
+            api.myWorkspaces()
             mutableState.value = State.Loaded(api.getAll().toList())
         }
     }
