3773879a0a96b309c235452f48955d9c26d2b6ed
Artem Kuznetsov <artem@invoid.vision>
2025-06-28 19:57:04 +0200

    feat(login): Improve login screen UI and UX
    Implement the following changes to the LoginScreen:
    
    - Remove the unnecessary `rememberCoroutineScope` import
    - Use a Column layout to structure the content of the screen
    - Add a logo and a title at the top of the screen
    - Use `PTextFieldEmail` and `PTextFieldPassword` components for the form fields
    - Align the form fields to the start of the screen
    - Improve the overall visual design and spacing of the screen
    
    These changes aim to enhance the user experience and visual appeal of the login screen, making it more intuitive and visually pleasing for the users.

--- composeApp/src/commonMain/kotlin/ai/thepredict/app/wrap/Themed.kt
+++ composeApp/src/commonMain/kotlin/ai/thepredict/app/wrap/Themed.kt
@@ -4,13 +4,14 @@ import ai.thepredict.ui.theme.createColorScheme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Themed(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = createColorScheme(useDarkTheme)
+    val colorScheme = createColorScheme(useDarkTheme).copy(background = Color(0xFFFFFFFF))
     MaterialTheme(colorScheme = colorScheme) {
         content()
     }
