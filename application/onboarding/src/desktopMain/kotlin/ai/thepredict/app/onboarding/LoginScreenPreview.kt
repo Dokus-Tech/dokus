9af4a1b587dee8831165f20a9eb4a573d3065d49
Artem Kuznetsov <artem@invoid.vision>
2025-06-29 13:31:30 +0200

    feat(onboarding): Implement desktop login screen layout
    This change introduces a new `LoginScreenDesktopContent` composable function that
    provides a desktop-specific layout for the login screen. The layout includes a
    left-aligned section with the app name and copyright information, and a
    centered section that contains the login form.
    
    The changes also include some minor adjustments to the `LoginForm` composable,
    such as removing the `fillMaxSize` modifier and adding a `widthIn` modifier to
    limit the form's width on desktop.
    
    These changes ensure that the login screen provides a consistent and
    well-designed user experience across both mobile and desktop platforms.

--- application/onboarding/src/desktopMain/kotlin/ai/thepredict/app/onboarding/LoginScreenPreview.kt
+++ application/onboarding/src/desktopMain/kotlin/ai/thepredict/app/onboarding/LoginScreenPreview.kt
@@ -1,7 +1,6 @@
 package ai.thepredict.app.onboarding
 
-import ai.thepredict.app.onboarding.authentication.login.LoginForm
-import ai.thepredict.app.onboarding.authentication.login.LoginScreen
+import ai.thepredict.app.onboarding.authentication.login.LoginScreenDesktopContent
 import ai.thepredict.ui.theme.createColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -11,10 +10,9 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun LoginScreenPreview() {
     val colorScheme = createColorScheme(false)
-    val screen = LoginScreen()
 
     MaterialTheme(colorScheme = colorScheme) {
-        LoginForm(
+        LoginScreenDesktopContent(
             email = "",
             onEmailChange = { /*TODO*/ },
             password = "",
