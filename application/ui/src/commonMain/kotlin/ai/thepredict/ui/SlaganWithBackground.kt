f66ceb8e2c3b341416f2de77243f6c78b9a92433
Artem Kuznetsov <artem@invoid.vision>
2025-07-01 21:43:05 +0200

    feat(ui): Add SloganWithBackground component
    This change introduces a new `SloganWithBackground` component that displays a slogan with a background gradient. The component is used in the `LoginScreen` to provide a visually appealing and consistent design across the application.
    
    The key changes include:
    
    - Create a new `SloganWithBackground` component in `SlaganWithBackground.kt` that renders a box with a background gradient and centers the slogan text within it.
    - Update the `LoginScreen` in `LoginScreen.kt` to use the new `SloganWithBackground` component instead of the previous custom implementation.
    - Add the `LocalFocusManager` to the `LoginScreenPreview` in both `LoginScreenPreview.kt` files to ensure the focus management works correctly.
    - Update the Xcode version in the `.run/Desktop[run].run.xml` file to match the current development environment.

---
+++ application/ui/src/commonMain/kotlin/ai/thepredict/ui/SlaganWithBackground.kt
@@ -0,0 +1,34 @@
+package ai.thepredict.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SloganWithBackground(modifier: Modifier = Modifier) {
+    Box(modifier) {
+        BackgroundGradientAnimated()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(40.dp), // Add padding around the text
+            contentAlignment = Alignment.Center // Center the text within the Box
+        ) {
+            Text(
+                text = "Financial forecasting without the headaches or jargon. Just smart insights that help your Belgian business thrive.",
+                color = MaterialTheme.colorScheme.onPrimary, // White text color
+                style = MaterialTheme.typography.headlineMedium, // Adjust text style as needed
+                fontWeight = FontWeight.Bold,
+                lineHeight = 40.sp // Adjust line height for better readability
+            )
+        }
+    }
+}
