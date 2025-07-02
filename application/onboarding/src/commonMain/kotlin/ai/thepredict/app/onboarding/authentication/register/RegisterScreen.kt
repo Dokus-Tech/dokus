94acc28d930f5a5c87485e759b9b88a77956eb74
Artem Kuznetsov <artem@invoid.vision>
2025-07-02 17:54:54 +0200

    feat(ui): Enhance CopyRightText component
    Adds a modifier parameter to the CopyRightText composable function
    to allow for customization of the text layout. This change
    enables better positioning and styling of the copyright text
    within the application UI.
    
    feat(onboarding): Redesign RegisterScreen
    
    Significantly refactors the RegisterScreen to improve the
    user experience and visual design. Key changes include:
    
    - Adds a SloganWithBackground component to display the app
      name and slogan in a visually appealing way.
    - Introduces a responsive layout that adjusts based on
      screen size, with a larger screen layout displaying the
      app name, slogan, and registration form side-by-side.
    - Enhances the registration form with improved field
      styling and error handling.
    - Adds a "Already have an account?" link to navigate to
      the LoginScreen.
    - Includes the CopyRightText component at the bottom of
      the screen.
    
    The goal of these changes is to create a more polished and
    user-friendly registration experience for the application.

--- application/onboarding/src/commonMain/kotlin/ai/thepredict/app/onboarding/authentication/register/RegisterScreen.kt
+++ application/onboarding/src/commonMain/kotlin/ai/thepredict/app/onboarding/authentication/register/RegisterScreen.kt
@@ -1,22 +1,35 @@
 package ai.thepredict.app.onboarding.authentication.register
 
+import ai.thepredict.app.core.constrains.isLargeScreen
 import ai.thepredict.app.navigation.OnboardingNavigation
+import ai.thepredict.app.onboarding.authentication.login.LoginViewModel
 import ai.thepredict.domain.exceptions.PredictException
-import ai.thepredict.ui.PButton
-import ai.thepredict.ui.PErrorText
-import ai.thepredict.ui.PTitle
-import ai.thepredict.ui.common.PTopAppBar
+import ai.thepredict.ui.SloganWithBackground
 import ai.thepredict.ui.fields.PTextFieldEmail
 import ai.thepredict.ui.fields.PTextFieldEmailDefaults
-import ai.thepredict.ui.fields.PTextFieldFreeDefaults
 import ai.thepredict.ui.fields.PTextFieldName
 import ai.thepredict.ui.fields.PTextFieldPassword
+import ai.thepredict.ui.text.AppNameText
+import ai.thepredict.ui.text.CopyRightText
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -26,106 +39,302 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.registry.rememberScreen
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import compose.icons.FeatherIcons
-import compose.icons.feathericons.User
-
-private val RegisterViewModel.State.exceptionOrNull: PredictException?
-    get() = when (this) {
-        is RegisterViewModel.State.Error -> exception
-        else -> null
-    }
 
 internal class RegisterScreen : Screen {
 
     @Composable
     override fun Content() {
-        val viewModel = rememberScreenModel { RegisterViewModel() }
+        val viewModel = rememberScreenModel { LoginViewModel() }
         val data = viewModel.state.collectAsState()
-        val fieldsError = data.value.exceptionOrNull
-
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var name by remember { mutableStateOf("") }
+        val fieldsError: PredictException? =
+            (data.value as? LoginViewModel.State.Error)?.exception
 
         val navigator = LocalNavigator.currentOrThrow
-        val workspacesOverview = rememberScreen(OnboardingNavigation.Workspaces.All)
-
         val focusManager = LocalFocusManager.current
 
-        Scaffold(
-            topBar = { PTopAppBar("Welcome to The Predict") }
-        ) { contentPadding ->
-            Column(
-                modifier = Modifier.fillMaxWidth().padding(contentPadding),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly,
+        val loginScreen = rememberScreen(OnboardingNavigation.Authorization.LoginScreen)
+
+        var fullName by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        val mutableInteractionSource = remember { MutableInteractionSource() }
+
+        Scaffold { contentPadding ->
+            Box(
+                Modifier
+                    .padding(contentPadding)
+                    .clickable(
+                        indication = null,
+                        interactionSource = mutableInteractionSource
+                    ) {
+                        focusManager.clearFocus()
+                    }
             ) {
-                PTitle("Let's create an account")
-
-                PTextFieldName(
-                    modifier = Modifier.padding(vertical = 16.dp),
-                    fieldName = "Name",
-                    error = fieldsError.takeIf { it is PredictException.InvalidName },
-                    value = name,
-                    icon = FeatherIcons.User,
-                    keyboardOptions = PTextFieldFreeDefaults.keyboardOptions.copy(imeAction = ImeAction.Next),
-                    onAction = { focusManager.moveFocus(FocusDirection.Next) }
-                ) {
-                    name = it
-                }
-
-                Spacer(modifier = Modifier.padding(vertical = 16.dp))
-
-                PTextFieldEmail(
-                    modifier = Modifier.padding(vertical = 16.dp),
-                    fieldName = "Email",
-                    error = fieldsError.takeIf { it is PredictException.InvalidEmail },
-                    value = email,
-                    keyboardOptions = PTextFieldEmailDefaults.keyboardOptions.copy(imeAction = ImeAction.Next),
-                    onAction = { focusManager.moveFocus(FocusDirection.Next) },
-                ) {
-                    email = it
-                }
-
-                PTextFieldPassword(
-                    fieldName = "Password",
-                    error = fieldsError.takeIf { it is PredictException.WeakPassword },
-                    value = password,
-                    onAction = { focusManager.clearFocus() }
-                ) {
-                    password = it
-                }
-
-                Spacer(modifier = Modifier.padding(vertical = 16.dp))
-
-                PButton("Register", modifier = Modifier.padding(vertical = 32.dp)) {
-                    viewModel.createUser(email, password, name)
-                }
-
-                Spacer(modifier = Modifier.weight(1f))
-
-                when (val state = data.value) {
-                    is RegisterViewModel.State.Loading -> {
-
-                    }
-
-                    is RegisterViewModel.State.Loaded -> {
-                        navigator.replace(workspacesOverview)
-                    }
-
-                    is RegisterViewModel.State.Error -> {
-                        PErrorText(state.exception)
-                    }
+                if (isLargeScreen) {
+                    RegisterScreenDesktopContent(
+                        focusManager = focusManager,
+                        email = email,
+                        onEmailChange = { email = it },
+                        password = password,
+                        onPasswordChange = { password = it },
+                        fullName = fullName,
+                        onFullNameChange = { fullName = it },
+                        fieldsError = fieldsError,
+                        onLoginClick = { navigator.replace(loginScreen) },
+                        onRegisterClick = { /* Handle register click */ },
+                        modifier = Modifier
+                    )
+                } else {
+                    RegisterScreenMobileContent(
+                        focusManager = focusManager,
+                        email = email,
+                        onEmailChange = { email = it },
+                        password = password,
+                        onPasswordChange = { password = it },
+                        fullName = fullName,
+                        onFullNameChange = { fullName = it },
+                        fieldsError = fieldsError,
+                        onLoginClick = { navigator.replace(loginScreen) },
+                        onRegisterClick = { /* Handle register click */ },
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+internal fun RegisterScreenMobileContent(
+    focusManager: FocusManager,
+    email: String,
+    onEmailChange: (String) -> Unit,
+    password: String,
+    onPasswordChange: (String) -> Unit,
+    fullName: String,
+    onFullNameChange: (String) -> Unit,
+    fieldsError: PredictException?,
+    onLoginClick: () -> Unit,
+    onRegisterClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+
+        // Logo
+        Text(
+            text = "Predict",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.displaySmall
+        )
+
+        RegisterForm(
+            focusManager = focusManager,
+            email = email,
+            onEmailChange = onEmailChange,
+            password = password,
+            onPasswordChange = onPasswordChange,
+            fieldsError = fieldsError,
+            onLoginClick = onLoginClick,
+            onRegisterClick = onRegisterClick,
+            fullName = fullName,
+            onFullNameChange = onFullNameChange,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Spacer(Modifier.weight(1f))
+    }
+}
+
+@Composable
+internal fun RegisterScreenDesktopContent(
+    focusManager: FocusManager,
+    email: String,
+    onEmailChange: (String) -> Unit,
+    password: String,
+    onPasswordChange: (String) -> Unit,
+    fullName: String,
+    onFullNameChange: (String) -> Unit,
+    fieldsError: PredictException?,
+    onLoginClick: () -> Unit,
+    onRegisterClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Row(Modifier.weight(1f).padding(32.dp)) {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.weight(2f).fillMaxHeight()
+            ) {
+                AppNameText()
+                CopyRightText()
+            }
+            // Center LoginForm vertically in desktop mode
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .weight(3f)
+                    .fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                RegisterForm(
+                    focusManager = focusManager,
+                    email = email,
+                    onEmailChange = onEmailChange,
+                    password = password,
+                    onPasswordChange = onPasswordChange,
+                    fullName = fullName,
+                    onFullNameChange = onFullNameChange,
+                    fieldsError = fieldsError,
+                    onLoginClick = onLoginClick,
+                    onRegisterClick = onRegisterClick,
+                    modifier = Modifier
+                )
+            }
+            Spacer(modifier = Modifier.weight(2f))
+        }
+        SloganWithBackground(Modifier.weight(1f))
+    }
+}
+
+@Composable
+internal fun RegisterForm(
+    focusManager: FocusManager,
+    email: String,
+    onEmailChange: (String) -> Unit,
+    password: String,
+    onPasswordChange: (String) -> Unit,
+    fullName: String,
+    onFullNameChange: (String) -> Unit,
+    fieldsError: PredictException?,
+    onLoginClick: () -> Unit,
+    onRegisterClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start, // Center content horizontally
+        verticalArrangement = Arrangement.Center // Center content vertically
+    ) {
+        // Title
+        Text(
+            text = "Login to account",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colorScheme.onBackground,
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Form fields
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            PTextFieldName(
+                fieldName = "Full Name",
+                error = fieldsError.takeIf { it is PredictException.InvalidName },
+                value = fullName,
+                keyboardOptions = PTextFieldEmailDefaults.keyboardOptions.copy(imeAction = ImeAction.Next),
+                onAction = { focusManager.moveFocus(FocusDirection.Next) },
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = onFullNameChange
+            )
+
+            PTextFieldEmail(
+                fieldName = "Email address",
+                error = fieldsError.takeIf { it is PredictException.InvalidEmail },
+                value = email,
+                keyboardOptions = PTextFieldEmailDefaults.keyboardOptions.copy(imeAction = ImeAction.Next),
+                onAction = { focusManager.moveFocus(FocusDirection.Next) },
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = onEmailChange
+            )
+
+            PTextFieldPassword(
+                fieldName = "Password",
+                value = password,
+                error = fieldsError.takeIf { it is PredictException.WeakPassword },
+                onAction = { focusManager.clearFocus() },
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = onPasswordChange
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Login Button
+        Button(
+            onClick = onLoginClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(38.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Text(
+                text = "Register",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Medium
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Sign up text - moved up here to match Figma
+        TextButton(
+            modifier = Modifier.align(Alignment.Start),
+            onClick = onRegisterClick
+        ) {
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        SpanStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Normal,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                    ) {
+                        append("Already have an account? ")
+                    }
+                    withStyle(
+                        SpanStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    ) {
+                        append("Login")
+                    }
+                },
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
     }
 }
