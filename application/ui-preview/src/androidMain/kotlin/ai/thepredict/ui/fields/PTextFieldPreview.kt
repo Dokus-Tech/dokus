28608e1a8473b5665cf65cc27d0052ea28059bb1
Artem Kuznetsov <artem@invoid.vision>
2025-07-03 16:30:04 +0200

    feat(ui-preview): Add Themed wrapper to previews
    The changes add a `Themed` wrapper around the `PTextFieldPreview` and `SectionTitlePreview` composables. This ensures that the previews are rendered with the correct theme and styling, providing a more accurate representation of the final UI.

--- application/ui-preview/src/androidMain/kotlin/ai/thepredict/ui/fields/PTextFieldPreview.kt
+++ application/ui-preview/src/androidMain/kotlin/ai/thepredict/ui/fields/PTextFieldPreview.kt
@@ -1,14 +1,17 @@
 package ai.thepredict.ui.fields
 
+import ai.thepredict.ui.Themed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun PTextFieldPreview() {
-    PTextFieldEmail(
-        fieldName = "Email address",
-        value = "",
-        onValueChange = { /* Handle value change */ }
-    )
+    Themed {
+        PTextFieldEmail(
+            fieldName = "Email address",
+            value = "",
+            onValueChange = { /* Handle value change */ }
+        )
+    }
 }
