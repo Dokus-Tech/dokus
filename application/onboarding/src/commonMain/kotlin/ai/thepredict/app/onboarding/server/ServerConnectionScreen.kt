f07395d40a7829b7711efacc73513e7bcaef5add
Artem Kuznetsov <artem@invoid.vision>
2025-07-03 15:07:21 +0200

    feat: Add ServerConnectionScreen and preview
    Adds a new `ServerConnectionScreen` and a preview for it. This is part
    of the onboarding flow, where the user can connect to a server. The
    screen is currently empty, but will be filled in with the necessary
    functionality in the future.

---
+++ application/onboarding/src/commonMain/kotlin/ai/thepredict/app/onboarding/server/ServerConnectionScreen.kt
@@ -0,0 +1,16 @@
+package ai.thepredict.app.onboarding.server
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+internal class ServerConnectionScreen : Screen {
+    @Composable
+    override fun Content() {
+        ServerConnectionScreenContent()
+    }
+}
+
+@Composable
+fun ServerConnectionScreenContent() {
+
+}
