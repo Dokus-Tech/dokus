173d531f20c6b422f9c289d471d24aebb51b4fab
Artem Kuznetsov <artem@invoid.vision>
2025-07-09 23:54:02 +0200

    feat(domain): remove unused `notes` field from `Contact`
    The `notes` field in the `Contact` model was unused and not required, so it has been removed to simplify the model and reduce unnecessary complexity.

--- shared/domain/src/commonMain/kotlin/ai/thepredict/domain/model/old/Contact.kt
+++ shared/domain/src/commonMain/kotlin/ai/thepredict/domain/model/old/Contact.kt
@@ -14,7 +14,6 @@ data class Contact(
     val email: String? = null,
     val taxNumber: String? = null,
     val companyName: String? = null,
-    val notes: List<Note> = emptyList(),
     val url: String? = null,
     val logo: String? = null,
 ) {
