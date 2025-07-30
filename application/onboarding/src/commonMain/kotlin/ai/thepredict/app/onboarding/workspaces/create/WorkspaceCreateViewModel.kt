2f506179de80198e3121b12d83e6ad261e606cea
Artem Kuznetsov <artem@invoid.vision>
2025-07-30 19:51:47 +0200

    feat(onboarding): Improve workspace creation screen
    - Simplify the workspace creation screen by removing unnecessary
      components and focusing on the core functionality
    - Add navigation to the home screen after successful workspace creation
    - Handle field errors and display them to the user
    - Improve the overall user experience by making the screen more
      responsive and intuitive

--- application/onboarding/src/commonMain/kotlin/ai/thepredict/app/onboarding/workspaces/create/WorkspaceCreateViewModel.kt
+++ application/onboarding/src/commonMain/kotlin/ai/thepredict/app/onboarding/workspaces/create/WorkspaceCreateViewModel.kt
@@ -7,29 +7,23 @@ import ai.thepredict.domain.usecases.CreateNewWorkspaceUseCase
 import ai.thepredict.repository.api.UnifiedApi
 import cafe.adriel.voyager.core.model.StateScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import org.kodein.di.instance
 
-internal class WorkspaceCreateViewModel : StateScreenModel<WorkspaceCreateViewModel.State>(State.Idle) {
+internal class WorkspaceCreateViewModel :
+    StateScreenModel<WorkspaceCreateViewModel.State>(State.Idle) {
 
     private val createNewWorkspaceUseCase: CreateNewWorkspaceUseCase by di.instance()
     private val api: UnifiedApi by di.instance { screenModelScope }
 
+    private val mutableEffect = MutableSharedFlow<Effect>()
+    val effect = mutableEffect.asSharedFlow()
+
     fun create(name: String, legalName: String, taxNumber: String) {
         screenModelScope.launch {
             mutableState.value = State.Loading
-
-            val newWorkspace = createNewWorkspaceUseCase(name, legalName, taxNumber).getOrElse {
-                mutableState.value = State.Error(it.asPredictException)
-                return@launch
-            }
-//
-//            val workspace = api.createWorkspace(newWorkspace).getOrElse {
-//                mutableState.value = State.Error(it.asPredictException)
-//                return@launch
-//            }
-//
-//            mutableState.value = State.Loaded(workspace)
         }
     }
 
@@ -38,8 +32,10 @@ internal class WorkspaceCreateViewModel : StateScreenModel<WorkspaceCreateViewMo
 
         data object Loading : State
 
-        data class Loaded(val workspace: Any) : State
-
         data class Error(val exception: PredictException) : State
+    }
+
+    sealed interface Effect {
+        data object NavigateToHome : Effect
     }
 }
