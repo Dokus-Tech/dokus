aade17be36af38a9ba0c0668c8f32100d47af26d
Artem Kuznetsov <artem@invoid.vision>
2025-07-31 12:47:39 +0200

    feat(domain): add new validators for workspace creation
    This commit introduces new validators for the workspace creation process. The changes include:
    
    - Added `ValidateNotShortUseCase` to validate that a string is not too short (less than 4 characters).
    - Added `ValidatePostalCode` to validate the postal code based on the country.
    - Refactored `ValidateNewWorkspaceUseCase` to use the new validators and handle address validation.
    - Added `ValidateAddressUseCase` to validate the different components of an address (street name, city, postal code, country).
    
    These changes aim to improve the validation logic for creating new workspaces, ensuring that the required fields meet the necessary criteria.

--- shared/domain/src/commonMain/kotlin/ai/thepredict/domain/usecases/validators/ValidateWorkspaceTaxNumberUseCase.kt
+++ shared/domain/src/commonMain/kotlin/ai/thepredict/domain/usecases/validators/ValidateWorkspaceTaxNumberUseCase.kt
@@ -4,6 +4,7 @@ import ai.thepredict.domain.model.TaxNumber
 
 
 class ValidateWorkspaceTaxNumberUseCase : Validator<String?> {
+
     override fun invoke(value: String?): Boolean {
         if (value.isNullOrEmpty()) return true
         return TaxNumber.canBeUsed(value)
