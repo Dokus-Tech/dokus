57a20686d2f5fe149305963b60dad272e4d84eff
Artem Kuznetsov <artem@invoid.vision>
2025-08-01 15:19:08 +0200

    feat: setup navigation bar and add preview
    This commit introduces the implementation of the navigation bar and adds a
    preview for it. The navigation bar is a key component of the application's
    user interface, providing users with a way to navigate between different
    sections of the app.
    
    The changes include:
    
    - Implementing the `NavigationBar` composable function in the
      `NavigationBar.kt` file, which will handle the rendering and behavior of
      the navigation bar.
    - Adding a preview for the `NavigationBar` composable in the
      `NavigationBar.kt` file in the `ui-preview` module, which allows
      developers to see how the navigation bar will look in the app.
    
    These changes are important as they lay the foundation for the application's
    navigation system, which is a crucial aspect of the user experience.

---
+++ application/ui-preview/src/androidMain/kotlin/ai/thepredict/ui/navigation/NavigationBar.kt
@@ -0,0 +1,13 @@
+package ai.thepredict.ui.navigation
+
+import ai.thepredict.ui.Themed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun NavigationBarPreview() {
+    Themed {
+        NavigationBar()
+    }
+}
---
+++ application/ui/src/commonMain/kotlin/ai/thepredict/ui/navigation/NavigationBar.kt
@@ -0,0 +1,8 @@
+package ai.thepredict.ui.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NavigationBar() {
+    // TODO: Implement the navigation bar
+}
