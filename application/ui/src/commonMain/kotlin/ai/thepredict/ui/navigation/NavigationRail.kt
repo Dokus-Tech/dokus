2401448124d2b8bb2995e82876ec648cbda6cef1
Artem Kuznetsov <artem@invoid.vision>
2025-08-04 19:49:39 +0200

    feat(ui): Implement Material Design 3 ripple effect
    This commit introduces the Material Design 3 ripple effect to the
    ColorScheme and NavigationRail components. The changes include:
    
    1. Added a `rippleColor` and `ripple` extension properties to the
       `ColorScheme` class to provide a consistent way of applying the
       ripple effect.
    
    2. Updated the `NavigationRail` component to use the new `ripple`
       effect when an item is clicked, providing a more visually
       appealing user experience.
    
    These changes improve the overall look and feel of the application,
    aligning it with the latest Material Design guidelines and
    providing a more polished user interface.

--- application/ui/src/commonMain/kotlin/ai/thepredict/ui/navigation/NavigationRail.kt
+++ application/ui/src/commonMain/kotlin/ai/thepredict/ui/navigation/NavigationRail.kt
@@ -1,7 +1,9 @@
 package ai.thepredict.ui.navigation
 
+import ai.thepredict.ui.theme.ripple
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -19,7 +21,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,7 +48,11 @@ fun ColumnScope.NavigationRail(
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable { onSelectedItemChange(item) },
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        onClick = { onSelectedItemChange(item) },
+                        indication = MaterialTheme.colorScheme.ripple
+                    ),
                 shape = RoundedCornerShape(8.dp),
                 colors = CardDefaults.cardColors(
                     containerColor = if (isSelected) {
