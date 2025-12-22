9b74438606683d0b0b4511ae451a57e95a252c58
Artem Kuznetsov <artem@invoid.vision>
2025-12-22 21:25:03 +0100

    fix: import

--- foundation/design-system/src/commonMain/kotlin/ai/dokus/foundation/design/style/Themed.kt
+++ foundation/design-system/src/commonMain/kotlin/ai/dokus/foundation/design/style/Themed.kt
@@ -4,9 +4,9 @@ import ai.dokus.foundation.design.local.LocalThemeManager
 import ai.dokus.foundation.platform.activePlatform
 import ai.dokus.foundation.platform.isWeb
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RippleAlpha
 import androidx.compose.material3.RippleConfiguration
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
