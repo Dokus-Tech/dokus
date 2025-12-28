908532159e7ca1a5693f473c044d7c39b13b2e42
Artem Kuznetsov <artem@invoid.vision>
2025-12-28 15:50:35 +0100

    Refactor OCR Gradle module dependencies and plugins

--- foundation/ocr/build.gradle.kts
+++ foundation/ocr/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
-    alias(libs.plugins.kotlinPluginSerialization)
 }
 
 group = "tech.dokus.ocr"
@@ -11,15 +10,5 @@ kotlin {
 }
 
 dependencies {
-    implementation(project.dependencies.platform(libs.koin.bom))
-    implementation(libs.koin.ktor)
-
-    implementation(libs.kotlinx.serialization)
-
-    // MinIO object storage
-    implementation(libs.minio)
-
-    // Image processing for avatar resizing
-    implementation(libs.scrimage.core)
-    implementation(libs.scrimage.webp)
+    testImplementation(libs.kotlin.test)
 }
