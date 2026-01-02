9fd31121d1285e1c7260c86703f916c7aeac4379
Artem Kuznetsov <artem@invoid.vision>
2026-01-02 14:57:51 +0100

    chore: localize provider

--- foundation/aura/src/commonMain/kotlin/tech/dokus/foundation/aura/extensions/PeppolProviderExtensions.kt
+++ foundation/aura/src/commonMain/kotlin/tech/dokus/foundation/aura/extensions/PeppolProviderExtensions.kt
@@ -1,6 +1,28 @@
 package tech.dokus.foundation.aura.extensions
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Receipt
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import org.jetbrains.compose.resources.stringResource
+import tech.dokus.aura.resources.Res
+import tech.dokus.aura.resources.peppol_provider_recommand_description
 import tech.dokus.domain.model.PeppolProvider
+
+val PeppolProvider.localized: String
+    @Composable get() = when (this) {
+        PeppolProvider.Recommand -> "Recommand"
+    }
+
+val PeppolProvider.description: String
+    @Composable get() = when (this) {
+        PeppolProvider.Recommand -> stringResource(Res.string.peppol_provider_recommand_description)
+    }
+
+val PeppolProvider.iconized: ImageVector
+    get() = when (this) {
+        PeppolProvider.Recommand -> Icons.Outlined.Receipt
+    }
 
 val PeppolProvider.websiteUrl: String
     get() = when (this) {
