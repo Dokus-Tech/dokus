fb5663a68942a975abdc544abcca4c9e0080ae86
Artem Kuznetsov <artem@invoid.vision>
2026-03-05 13:12:11 +0100

    Merge main and fix build

--- backendApp/src/main/kotlin/tech/dokus/backend/services/documents/DocumentTruthService.kt
+++ backendApp/src/main/kotlin/tech/dokus/backend/services/documents/DocumentTruthService.kt
@@ -7,7 +7,6 @@ import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.experimental.newSuspendedTransaction
-import tech.dokus.backend.util.runSuspendCatching
 import tech.dokus.database.repository.cashflow.DocumentBlobCreatePayload
 import tech.dokus.database.repository.cashflow.DocumentBlobRepository
 import tech.dokus.database.repository.cashflow.DocumentDraftRepository
